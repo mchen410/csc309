c853fef9a68a98f12cca4e076a2ce7bad9a4a129
Leng <maryelaine.malit@gmail.com>
2013-02-06 15:45:19 -0500

    Initiliazed repo and Added files

---
+++ index.js
@@ -0,0 +1,345 @@
+var numTopics = 0;
+var topicOpen = 0;
+var commentOpen = 0;
+var numComments = 0;
+// var upvotes = new Array();
+
+// Sorting topics by votes. TODO. Sort comments by votes.
+jQuery.fn.sort = function() {
+    return this.pushStack([].sort.apply(this, arguments), []);
+};
+
+function sortByTotalVotes(a, b){
+    if (a.totalVotes == b.totalVotes){
+        return 0;
+    }
+    return a.totalVotes > b.totalVotes ? 1 : -1;
+};
+
+function sortByTotalVotesDesc(a, b){
+    return sortByTotalVotes(a, b) * -1;
+};
+
+/**RENDER ELEMENTS FROM SERVER**/
+
+//Render the elements on the page
+function loadPage()
+{
+	$.getJSON('topics', function(json) {
+        var sortedTopics = $(json.topics).sort(sortByTotalVotesDesc);
+		$.each(sortedTopics, function(i, topic) {
+			renderTopic(topic.id, topic);
+			loadComments(topic.id);
+		});
+	});
+};
+
+//Display the topic with id i.
+function renderTopic(i, topic)
+{
+	//Create a new div to populate inside topicsList
+	var topicDiv = document.createElement('div');
+	topicDiv['id'] = 'topic' + i;
+	topicDiv.className = 'topic';
+	$(topicDiv).appendTo('#topicsList');
+
+    // Text and link
+	$('<a class="topicTitle"/>').attr('id', 'link' + i).appendTo(topicDiv);
+	document.getElementById('link' + i).innerHTML = topic.title + '<br/>';
+	
+	$('<a class="topicLink" href="' + topic.link + '"> ' + topic.link + '</a> <br/>').appendTo(topicDiv);
+
+    // Num votes
+    $("<div/>", {
+        id: "votes" + i,
+        href: "#",
+        onclick: "upboat('" + i + "')", // Only supporting up voting. Down vote opt.
+        class: "votes",
+        html: topic.votes,
+    }).appendTo("#topic" + i);
+
+    // Num comments
+	document.getElementById('topic' + i).innerHTML += 'number of comments: ' + topic.numComments + '<br/>';
+
+	//Create a button for posting a comment
+	$('<button id="postB' + i + '" class="postComment" onClick=commentBox()>Post a Comment</button>').appendTo(topicDiv);
+	$('<button id="showB' + i + '" class="showComment">Show Comments</button>').appendTo(topicDiv);
+	var pb = document.getElementById('postB' + i);
+	pb.setAttribute('onclick', 'commentBox(' + topicDiv + ')');
+	
+	//Create div container for elements
+	var container = document.createElement('div');
+	container['id'] = 'cmmtContainer' + i;
+	container.className = 'cmmtContainer';
+	$(container).appendTo(topicDiv);
+	
+	//Set the onclick function of Show Comments button
+	//We need comment container to do this
+	document.getElementById('showB' + i).setAttribute('onclick', 'showComments("cmmtContainer' + i + '")');
+}
+
+function upboat(postID){
+    $("#votes" + postID).html(parseInt($("#votes" + postID).html()) + 1);
+    $.post("/vote/" + postID, function(res){
+        // TODO. Do something opt.
+	});
+}
+// /*
+//  */
+// function upvote(topicId)
+// {
+// 	if (upvotes[topicId] == 1) {
+// 		var karmaId = 'karma' + topicId;
+// 		document.getElementById(karmaId).innerHTML = parseInt(document.getElementById(karmaId).innerHTML) - 1;
+// 		upvotes[topicId] = 0
+// 	} else {
+// 		var karmaId = 'karma' + topicId;
+// 		document.getElementById(karmaId).innerHTML = parseInt(document.getElementById(karmaId).innerHTML) + 1;
+// 		upvotes[topicId] = 1
+// 	}
+// }
+
+//Acquire from the server all the comments of the topic with topicID.
+function loadComments(topicID)
+{
+	var str = '/topic/' + topicID + '/children'
+	$.getJSON(str, function(data) {
+		//retrieve the comments from data dict
+		var cmmtDict = data.urlVar3;
+
+		if (!jQuery.isEmptyObject(cmmtDict)){
+			//create a div that contains all comments for this topic
+			var container = document.getElementById('cmmtContainer' + topicID);
+
+			//now disable the onclick on the topic's title, to prevent
+			//duplication of comment loads; once comments show, they
+			//will not retract
+			$('#link' + topicID).removeAttr('onclick');
+
+			var depth=1;
+			//render each comment
+			$.each(cmmtDict, function(key, comment){
+				renderComment(comment, container, depth);
+			});
+		}
+
+	});
+}
+
+// Render each comment, and add commenting functionality on each.
+function renderComment(comment, container, depth)
+{
+	//create a div for each individual comment
+	var cmmtDiv = document.createElement('div');
+	var divID = comment.id; //set divID as the path to the comment (stored in comment id), for easier storage at the backend
+	cmmtDiv['id'] = divID;
+	cmmtDiv.className = 'cmmtDiv';
+	$(cmmtDiv).appendTo(container);
+
+	//manually indent to give illusion of nesting
+	var leftMargin = depth * 20;
+	cmmtDiv.style.marginLeft='' + leftMargin + 'px';
+
+	//show the text and the number of votes
+	cmmtDiv.innerHTML += comment.text + '<br/>';
+
+    // Num votes
+    $("<div/>", {
+        id: "votes" + comment.id,
+        href: "#",
+        onclick: "upfloat('" + comment.id + "')", // Only supporting up voting. Down vote opt.
+        class: "votes",
+        html: comment.votes,
+    }).appendTo(cmmtDiv);
+	// cmmtDiv.innerHTML += 'Number of votes: ' + comment.votes + '<br/>';
+	// create an upvote button
+	// $('<button id="voteB' + divID + '" class="cmmtVote" onClick=upVote()>Vote!</button>').appendTo(cmmtDiv);
+
+	//create a comment button, to allow commenting on comments
+	$('<button id="cmmtB' + divID + '" class="cmmtComment">Post a Comment</button>').appendTo(cmmtDiv);
+	document.getElementById('cmmtB' + divID).setAttribute('onclick', 'commentBox("' + divID + '")');
+	$('<button id="showB' + divID + '" class="showComment">Show Comments</button>').appendTo(cmmtDiv);
+	document.getElementById('showB' + divID).setAttribute('onclick', 'showComments("' + divID + '")');
+	$(cmmtDiv).hide();
+	
+	//If this comment has comments, render its children
+	if (!jQuery.isEmptyObject(comment.children)){
+		$.each(comment.children, function(key, childCmmt){
+			renderComment(childCmmt, cmmtDiv, depth+1);
+		});
+	}
+}
+
+function showComments(divID){
+	//get the element with id divID; may either be a topic or a comment
+	var element = document.getElementById(divID);
+
+	//get its comment children;
+	$("div#" + divID + " > .cmmtDiv").show();
+	
+	//change the button to Hide Button
+	var button = document.getElementById('showB' + divID);
+	$("#showB"+divID).attr('value', 'Hide Comments');
+	button.innerHTML = 'Hide Comments';
+}
+
+function hideComments(){
+
+}
+
+
+function upfloat(postID){
+    // Don't know why jquery selection doesn't work here, like in
+    // upboat: $("#votes" + postID).html() returns undefined.
+    document.getElementById("votes" + postID).innerHTML = parseInt(document.getElementById("votes" + postID).innerHTML) + 1;
+    $.post("/vote/" + postID, function(res){
+        // TODO. Do something opt.
+	});
+}
+
+/**RENDER ELEMENTS TO ALLOW POST TO SERVER**/
+
+/* The client clicks on "Submit a topic" button, to submit a new topic to the server.
+ * The function generates textboxes for inserting the title and the link of the topic
+ * and append them to the topicActivated div.
+ * Moreover, it generates the "Submit your topic" and "Cancel Submission" buttons. */
+
+function wantToPost()
+{
+	if (topicOpen == 0) {
+		var division = document.createElement('div');
+		division['id'] = 'topicActivated';
+		document.getElementById('NewTopics').appendChild(division);
+
+		/* Create a textbox for the topic title. */
+        var titleBox = document.createElement('textarea');
+		titleBox['id'] = 'topicTitle';
+		titleBox['cols'] = '50';
+		titleBox['rows'] = '3';
+		document.getElementById('topicActivated').appendChild(titleBox);
+
+		document.getElementById('topicActivated').innerHTML += '<br />';
+
+		/* Create a textbox for the topic link. */
+        var linkBox = document.createElement('textarea');
+		linkBox['id'] = 'topicLink';
+		linkBox['cols'] = '50';
+		linkBox['rows'] = '1';
+		document.getElementById('topicActivated').appendChild(linkBox);
+
+		document.getElementById('topicActivated').innerHTML += '<br />';
+
+        /* Submit the topic. */
+		document.getElementById('topicActivated').innerHTML += '<a href="#" id="topicSubmission" onclick="submitTopic()">Submit your Topic</a>&emsp;&emsp;';
+
+        /* Cancel the active submission process. */
+		document.getElementById('topicActivated').innerHTML += '<a href="#" id="topicCancellation" onclick="cancelTopic()">Cancel Submission</a>';
+
+		topicOpen = 1;
+	}
+}
+
+//Removes text box for posting topics.
+function cancelTopic()
+{
+	var topic = document.getElementById('topicActivated');
+	topic.parentNode.removeChild(topic);
+	topicOpen = 0;
+}
+
+// This function provides a textbox to write a comment.
+function commentBox(divID)
+{
+	//To prevent more than one comment box to appear at the same time;
+	//Technically there could be a comment box for each comment / topic, but we're assuming
+	//one client is only worried about commenting on 1 thing at a time.
+	if (commentOpen == 0){
+		//Create a div in which to store the box
+		var division = document.createElement('div');
+		division['id'] = 'commentActivated';
+		
+		//This box must appear directly below the comment/topic the user is commenting on
+		var countChildren = $('#' + divID + '> .cmmtDiv').length;
+		if (countChildren > 0){
+			//If topic/comment has children, box must appear *before* first child
+			var firstCmmt = document.getElementById(divID + 'x0');
+			var thisCmmt = document.getElementById(divID);
+			thisCmmt.insertBefore(division, firstCmmt);
+		} else {
+			//If not, it can just be appended
+			document.getElementById(divID).appendChild(division);
+		}
+
+		//Create text area
+		var commentBox = document.createElement('textarea');
+		commentBox['id'] = 'comment';
+		commentBox['cols'] = '50';
+		commentBox['rows'] = '3';
+		division.appendChild(commentBox);
+		
+		//Create links to submit comment / cancel comment
+		division.innerHTML += '<br />';
+		division.innerHTML += '<a href="#" id="commentSubmission">Submit Your Comment</a>&emsp;&emsp;';
+		division.innerHTML += '<a href="#" id="commentCancel">Cancel Comment</a>&emsp;&emsp;';
+		document.getElementById('commentSubmission').setAttribute('onclick', 'submitComments("' + divID + '")');
+		document.getElementById('commentCancel').setAttribute('onclick', 'cancelComment()');
+
+		commentOpen = 1;
+	}
+}
+
+//Removes text box for posting topics.
+function cancelComment()
+{
+	var cmmtBox = document.getElementById('commentActivated');
+	cmmtBox.parentNode.removeChild(cmmtBox);
+	commentOpen = 0;
+}
+
+//Submit the topic inside the user input boxes
+function submitTopic()
+{
+	var link = document.getElementById('topicLink').value;
+	var title = document.getElementById('topicTitle').value;
+
+    /* Check that the provided link represents an actual link. */
+	if ((link.substring(0, 7) !== 'http://') || title.length == 0) {
+		alert('Invalid topic! Please revise.');
+	} else {
+		numTopics += 1;
+		cancelTopic();
+
+		//send new topic to the server, then render the new topic that was just sent
+		var data = 'topicTitle=' + title + '&topicLink=' + link;
+		$.post('/post/topic', data, function(response){
+			renderTopic(response.id, response);
+		}, 'json');
+	}
+}
+
+//Submit the comment inside the comment box
+function submitComments(path) {
+    var comment = document.getElementById('comment').value;
+    var data = 'commentText=' + comment + '&commentPath=' + path;
+	cancelComment();
+    $.post('/post/comment', data, function(response) {
+		   var pathList = response.id.split('x');
+		   //keep track of nesting!!
+		   var depth = pathList.length - 1; 
+		   //this is the div of the comment's parent
+		   var containerStr = response.id.substring(0, response.id.length - 2);
+		   
+		   if (pathList.length == 2) //this is a direct comment to a post
+		   {
+				//Topic divs = 'topic' + id; as opposed to comment divs
+				//which are just paths. REMEMBER TO CHANGE!!!! 
+				containerStr = 'topic' + pathList[0];
+		   }
+		   var container = document.getElementById(containerStr);
+		   renderComment(response, container, depth);
+           }, 'json');
+}
+
+// This function runs when the page is ready to load the existing topics.
+function loadTopics() {
+}
---
+++ server.js
@@ -0,0 +1,299 @@
+http = require('http');
+var fs = require('fs');
+path = require('path');
+var utils = require('util');
+var qs = require('querystring');
+var URL = require('url');
+
+topicsDatabase = {
+    "topics": [
+        {
+			"title": "This is my zeroth topic",
+			"link": "http://neopets.com",
+			"id" : "0",
+			"votes": 4,
+			"totalVotes" : 265,
+			"numComments" : 3,
+			"children": [
+				{
+					"text": "This is my zeroth comment",
+					"votes": 234,
+					"id" : "0x0",
+					"totalVotes" : 261,
+					"children": [
+						{
+							"text": "This is my first comment",
+							"votes": 23,
+							"id" : "0x0x0",
+							"totalVotes" : 23,
+							"children": []
+						},
+						{
+							"text": "This is my second comment",
+							"votes": 4,
+							"id" : "0x0x1",
+							"totalVotes" : 4,
+							"children": []
+						}
+					]
+				}
+			]
+		},
+		{
+			"title": "Going to heroku.com",
+			"link": "http://heroku.com",
+			"id" : "1",
+			"votes": 5,
+			"totalVotes" : 240,
+			"numComments" : 4,
+			"children": [
+				{
+					"text": "comment!!",
+					"votes": 2,
+					"id" : "1x0",
+					"totalVotes" : 225,
+					"children": [
+						{
+							"text": "Comment 6",
+							"votes": 223,
+							"totalVotes" : 223,
+							"id" : "1x0x0",
+							"children": []
+						}
+					]
+				},
+				{
+					"text": "comment!!",
+					"votes": 10,
+					"id" : "1x1",
+					"totalVotes" : 10,
+					"children": [
+						{
+							"text": "Comment 7",
+							"votes": 0,
+							"totalVotes" : 0,
+							"id" : "1x1x0",
+							"children": []
+						}
+					]
+				}
+			]
+		}
+	]
+};
+
+handler = function (req, res) {
+    url = URL.parse(req.url);
+    console.log('Request: ' + req.url);
+
+    if (req.url == '/') {
+        fs.readFile('./index.html', function (error, content) {
+            if (error) {
+                res.writeHead(200);
+                console.log(error);
+                res.end('<font color="red">404! Can\'t read index.html</font>')
+            } else {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                res.end(content, 'utf-8');
+            }
+        });
+    } else if (req.method == 'GET') {
+        path = url.pathname.split('/');
+        urlVar1 = path[path.length - 3];
+        urlVar2 = path[path.length - 2];
+        urlVar3 = path[path.length - 1];
+
+        if (urlVar1 == 'topic') {
+            topics = topicsDatabase.topics[urlVar2][urlVar3];
+
+            var returnValue = {
+                urlVar3: topics
+            };
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify(returnValue));
+
+        } else if (urlVar3 == 'topics') {
+            var returnValue = {
+                "topics": topicsDatabase
+            };
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify(topicsDatabase));
+
+        } else if (urlVar3 == "index.js"){
+            fs.readFile('./index.js', function (error, content) {
+                if (error) {
+                    res.writeHead(200);
+                    res.end('<font color="red">index.js: 404!</font>')
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/javascript'
+                    });
+                    res.end(content, 'utf-8');
+                }
+            });
+        } else if (urlVar3 == "style.css"){
+            fs.readFile('./style.css', function (error, content) {
+                    if (error) {
+                        res.writeHead(200);
+                        res.end('<font color="red">style.css: 404!</font>')
+                    } else {
+                        res.writeHead(200, {
+                                      'Content-Type': 'text/css'
+                        });
+                        res.end(content, 'utf-8');
+                    }
+            });
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end("404!");
+        }
+
+    } else if (req.method == 'POST' && req.url == '/post/topic') {
+
+        console.log("[200] " + req.method + " to " + req.url);
+        var postTopicData = '';
+
+        req.on('data', function (chunk) {
+            // append the current chunk of data to the postTopicData variable
+            postTopicData += chunk.toString();
+			console.log(postTopicData);
+        });
+
+        req.on('end', function () {
+            // request ended -> do something with the data
+            res.writeHead(200, "OK", {
+                'Content-Type': 'text/html'
+            });
+
+            // Format: topicTitle, topicLink, submit
+            var postTopic = qs.parse(postTopicData);
+
+            var newTopic = {
+                "title": postTopic.topicTitle,
+                "link": postTopic.topicLink,
+                "id" : topicsDatabase.topics.length,
+                "votes": 0,
+                "totalVotes" : 0,
+                "numComments" : 0,
+                "children": []
+            };
+            topicsDatabase.topics.push(newTopic);
+
+            //Debug
+            //res.write('<html><head><title>Post data</title></head><body><pre>');
+            //res.write(utils.inspect(topicsDatabase));
+            //res.write('</pre></body></html>');
+            //res.end();
+
+			console.log(newTopic);
+
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(newTopic));
+        });
+
+    } else if (req.method == 'POST' && req.url == '/post/comment') {
+        console.log("[200] " + req.method + " to " + req.url);
+        var postCommentData = '';
+
+        req.on('data', function (chunk) {
+            // append the current chunk of data to the postTopicData variable
+			postCommentData += chunk.toString();
+			console.log(postCommentData);
+        });
+
+        req.on('end', function () {
+            // commentText, commentTopic, commentPath, submit
+            var postComment = qs.parse(postCommentData);
+
+			//path example: 1:3:5:6, with 1 being the id of the topic
+			//and 6 being the direct parent of the new comment
+			cPath = postComment.commentPath.split('x');
+			console.log(cPath[0]);
+
+			var newComment = {
+				"text" : postComment.commentText,
+				"votes" : 0,
+                "totalVotes" : 0,
+				"id" : "",
+				"children" : []
+			};
+
+			//If comment is top level
+			var parentNode = topicsDatabase.topics[parseInt(cPath[0])];
+			parentNode.numComments += 1;
+			if (cPath.length != 1) {
+				for (var i = 1; i < cPath.length; i++) {
+					parentNode = parentNode["children"][parseInt(cPath[i])];
+				}
+			}
+			newComment.id = postComment.commentPath + 'x' + parentNode.children.length;
+			parentNode["children"].push(newComment);
+
+            //Debug
+            //res.write('<html><head><title>Post data</title></head><body><pre>');
+            //res.write(utils.inspect(topicsDatabase.topics));
+            //res.write('<br />');
+            //res.write(utils.inspect(topicsDatabase["topics"][parseInt(postComment.commentTopic)]["comments"]));
+            //res.write('</pre></body></html>');
+
+			console.log(newComment);
+
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(newComment));
+        });
+
+    } else if (req.method == 'POST') {
+        // Counting votes
+        var path = url.pathname.split('/');
+
+        if (path[path.length - 2] != 'vote') {
+            console.log("[404] " + req.method + " to " + req.url);
+            res.writeHead(404, "Page Not Found", {
+                'Content-Type': 'text/html'
+            });
+            res.end('<html><head><title>404</title></head><body><h1>404 Page Not Found</h1></body></html>');
+        }
+
+		var itemPath = path[path.length - 1];
+		var cPath = itemPath.split('x');
+		var nodeUpvoted = topicsDatabase.topics[parseInt(cPath[0])];
+
+		//If upvoted item is a comment
+		if (cPath.length != 1) {
+			for (var i = 1; i < cPath.length; i++) {
+				nodeUpvoted = nodeUpvoted.children[parseInt(cPath[i])];
+			}
+		}
+
+		//Increment votes by 1
+		nodeUpvoted.votes += 1;
+		nodeUpvoted.totalVotes += 1;
+
+		console.log(nodeUpvoted);
+
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(nodeUpvoted));
+
+    } else {
+        console.log("[404] " + req.method + " to " + req.url);
+        res.writeHead(404, "Page Not Found", {
+            'Content-Type': 'text/html'
+        });
+        res.end('<html><head><title>404</title></head><body><h1>404 Page Not Found</h1></body></html>');
+    }
+};
+
+host = '127.0.0.1';
+port = 30975;
+
+http.createServer(handler).listen(port);
+
+console.log('Server running at http://127.0.0.1:' + port + '/. Go check it out!');
