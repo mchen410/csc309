03f4a408524e303c31d5b50f2c35dd7ebad5f8bb
Aneeta Lutfee <aneeta.lutfee@mail.utotonto.ca>
2013-02-09 17:12:31 -0500

    fixed index problem

--- Script.sh
+++ Script.sh
@@ -50,75 +50,75 @@ curl --data "" http://localhost:30975/vote/3
 
 
 # add comments to first topic
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic&commentPath=1" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic&commentPath=1" http://localhost:30975/post/comment
-curl --data "commentText=my%20third%20comment%20on%20my%20first%20topic&commentPath=1" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20first%20topic&commentPath=1" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
+curl --data "commentText=my%20third%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
+curl --data "commentText=my%20forth%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
 
 # add vote to comments of first topic
 
 
 
 # add comments to second topic
-curl --data "commentText=my%20first%20comment%20on%20my%20second%20topic&commentPath=2" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20second%20topic&commentPath=2" http://localhost:30975/post/comment
-curl --data "commentText=my%20third%20comment%20on%20my%20second%20topic&commentPath=2" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20second%20topic&commentPath=2" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
+curl --data "commentText=my%20third%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
+curl --data "commentText=my%20forth%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
 
 # add comments to third topic
-curl --data "commentText=my%20first%20comment%20on%20my%20third%20topic&commentPath=3" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20third%20topic&commentPath=3" http://localhost:30975/post/comment
-curl --data "commentText=my%20third%20comment%20on%20my%20third%20topic&commentPath=3" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20third%20topic&commentPath=3" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
+curl --data "commentText=my%20third%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
+curl --data "commentText=my%20forth%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
 
 
 # add comments to forth topic
-curl --data "commentText=my%20first%20comment%20on%20my%20forth%20topic&commentPath=4" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20forth%20topic&commentPath=4" http://localhost:30975/post/comment
-curl --data "commentText=my%20third%20comment%20on%20my%20forth%20topic&commentPath=4" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20forth%20topic&commentPath=4" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
+curl --data "commentText=my%20third%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
+curl --data "commentText=my%20forth%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
 
 
 # Grenerate nested comments
 
 # add comments to the first topics first comment
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic's%20first%20comment&commentPath=1x0" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic's%20first%20comment&commentPath=1x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic's%20first%20comment&commentPath=0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic's%20first%20comment&commentPath=0x0" http://localhost:30975/post/comment
 
 # nested comment (third level)
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=1x0x0" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=1x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=0x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=0x0x0" http://localhost:30975/post/comment
 
 
 # nested comment (forth level)
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=1x0x0x0" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=1x0x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x0x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x0x0x0" http://localhost:30975/post/comment
 
 
 # nested comment (fifth level)
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=1x0x0x0x0" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=1x0x0x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=0x0x0x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=0x0x0x0x0" http://localhost:30975/post/comment
 
 
 # Grenerate nested comments
 
 # add comments to the first topics second comment
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic's%20second%20comment&commentPath=1x1" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic's%20second%20comment&commentPath=1x1" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic's%20second%20comment&commentPath=0x1" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic's%20second%20comment&commentPath=0x1" http://localhost:30975/post/comment
 
 # nested comment (third level)
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=1x1x0" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=1x1x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=0x1x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=0x1x0" http://localhost:30975/post/comment
 
 
 # nested comment (forth level)
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=1x1x0x0" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=1x1x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x1x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x1x0x0" http://localhost:30975/post/comment
 
 
 # nested comment (fifth level)
-curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=1x1x0x0x0" http://localhost:30975/post/comment
-curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=1x1x0x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=0x1x0x0x0" http://localhost:30975/post/comment
+curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=0x1x0x0x0" http://localhost:30975/post/comment
 
 
 
