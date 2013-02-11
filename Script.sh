6d508daa996b62130f8dce9092678903651b64c9
mchen410 <mchen410@gmail.com>
2013-02-10 19:30:51 -0500

    Update Script.sh
    Added urls for upvoting nested comments and top level comments of the first and second topic

--- Script.sh
+++ Script.sh
@@ -8,7 +8,7 @@ echo "this is a test script to populate the database"
 curl --data "topicTitle=my%20first%20topic&topicLink=http://yahoo.com" http://localhost:30975/post/topic
 curl --data "topicTitle=my%20second%20topic&topicLink=http://hotmail.com" http://localhost:30975/post/topic
 curl --data "topicTitle=my%20third%20topic&topicLink=http://twitter.com" http://localhost:30975/post/topic
-curl --data "topicTitle=my%20forth%20topic&topicLink=http://facebook.com" http://localhost:30975/post/topic
+curl --data "topicTitle=my%20fourth%20topic&topicLink=http://facebook.com" http://localhost:30975/post/topic
 
 
 # vote on topics
@@ -53,31 +53,57 @@ curl --data "" http://localhost:30975/vote/3
 curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
 curl --data "commentText=my%20third%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
+curl --data "commentText=my%20fourth%20comment%20on%20my%20first%20topic&commentPath=0" http://localhost:30975/post/comment
 
-# add vote to comments of first topic
+# upvote comments of first topic
+curl --data "" http://localhost:30975/vote/0x0
+curl --data "" http://localhost:30975/vote/0x0
+curl --data "" http://localhost:30975/vote/0x0
 
+curl --data "" http://localhost:30975/vote/0x1
+curl --data "" http://localhost:30975/vote/0x1
 
+curl --data "" http://localhost:30975/vote/0x2
+
+curl --data "" http://localhost:30975/vote/0x3
+curl --data "" http://localhost:30975/vote/0x3
+curl --data "" http://localhost:30975/vote/0x3
+curl --data "" http://localhost:30975/vote/0x3
+curl --data "" http://localhost:30975/vote/0x3
+curl --data "" http://localhost:30975/vote/0x3
 
 # add comments to second topic
 curl --data "commentText=my%20first%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
 curl --data "commentText=my%20third%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
+curl --data "commentText=my%20fourth%20comment%20on%20my%20second%20topic&commentPath=1" http://localhost:30975/post/comment
+
+# upvote comments of second topic
+curl --data "" http://localhost:30975/vote/1x0
+curl --data "" http://localhost:30975/vote/1x0
+
+curl --data "" http://localhost:30975/vote/1x1
+
+curl --data "" http://localhost:30975/vote/1x2
+curl --data "" http://localhost:30975/vote/1x2
+curl --data "" http://localhost:30975/vote/1x2
+curl --data "" http://localhost:30975/vote/1x2
+
+curl --data "" http://localhost:30975/vote/1x3
+curl --data "" http://localhost:30975/vote/1x3
+curl --data "" http://localhost:30975/vote/1x3
 
 # add comments to third topic
 curl --data "commentText=my%20first%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
 curl --data "commentText=my%20third%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
+curl --data "commentText=my%20fourth%20comment%20on%20my%20third%20topic&commentPath=2" http://localhost:30975/post/comment
 
-
-# add comments to forth topic
+# add comments to fourth topic
 curl --data "commentText=my%20first%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
 curl --data "commentText=my%20third%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
-curl --data "commentText=my%20forth%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
-
+curl --data "commentText=my%20fourth%20comment%20on%20my%20forth%20topic&commentPath=3" http://localhost:30975/post/comment
 
 # Grenerate nested comments
 
@@ -89,8 +115,7 @@ curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic's%20fir
 curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=0x0x0" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=0x0x0" http://localhost:30975/post/comment
 
-
-# nested comment (forth level)
+# nested comment (fourth level)
 curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x0x0x0" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x0x0x0" http://localhost:30975/post/comment
 
@@ -111,7 +136,7 @@ curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested
 curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20three%20levels&commentPath=0x1x0" http://localhost:30975/post/comment
 
 
-# nested comment (forth level)
+# nested comment (fourth level)
 curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x1x0x0" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20four%20levels&commentPath=0x1x0x0" http://localhost:30975/post/comment
 
@@ -120,13 +145,16 @@ curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20neste
 curl --data "commentText=my%20first%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=0x1x0x0x0" http://localhost:30975/post/comment
 curl --data "commentText=my%20second%20comment%20on%20my%20first%20topic%20nested%20five%20levels&commentPath=0x1x0x0x0" http://localhost:30975/post/comment
 
+# upvote nested comments
+curl --data "" http://localhost:30975/vote/0x1x0x0x0x1
+curl --data "" http://localhost:30975/vote/0x1x0x0x0x1
 
+curl --data "" http://localhost:30975/vote/0x1x0x0x0
+curl --data "" http://localhost:30975/vote/0x1x0x0x0
 
+curl --data "" http://localhost:30975/vote/0x1x0x0
 
-# add comments to comments
-
-
-# upvote topics
-
-
-# upvote comments
+curl --data "" http://localhost:30975/vote/0x1x0
+curl --data "" http://localhost:30975/vote/0x1x0
+curl --data "" http://localhost:30975/vote/0x1x0
+curl --data "" http://localhost:30975/vote/0x1x0
