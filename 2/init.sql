20af84e36d7c01016ecc592235f1ebeee12c1e0e
Trong Truong <nahn.truong@gmail.com>
2013-02-23 21:35:02 -0500

    force adding express

--- 2/init.sql
+++ 2/init.sql
@@ -1,17 +1,17 @@
 CREATE SCHEMA IF NOT EXISTS `csc309h_g1malitm`;
 USE `csc309h_g1malitm`;
-GRANT ALL ON *.* to 'g1malitm'@'localhost' IDENTIFIED BY 'tahziehe';
-FLUSH PRIVILEGES;
+-- GRANT ALL ON *.* to 'g1malitm'@'localhost' IDENTIFIED BY 'tahziehe';
+-- FLUSH PRIVILEGES;
 
 -- -----------------------------------------------------
 -- Create tables for blogs, user-post relationship, and posts
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `usersLikedPost`;
 DROP TABLE IF EXISTS `tracks`;
+DROP TABLE IF EXISTS `likedPosts`;
 DROP TABLE IF EXISTS `posts`;
 DROP TABLE IF EXISTS `blogs`;
 
-CREATE TABLE IF NOT EXISTS blogs( 
+CREATE TABLE IF NOT EXISTS blogs(
 	blogID		INT NOT NULL AUTO_INCREMENT,
 	blogName 	varchar(124),
 	PRIMARY KEY (blogID)
