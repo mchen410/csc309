d91d7018298f2351ec1bf98c8794829bf5263362
Leng <maryelaine.malit@gmail.com>
2013-02-26 20:19:54 -0500

    addBlog in blogs.js

--- 2/blogs.js
+++ 2/blogs.js
@@ -5,19 +5,36 @@ exports.frontDesk = function(req, res){
     res.send("hello NEO, the ORACLE has been expecting you....");
 }
 
-// todo. parse incoming urls and call more methods
-// todo. parse incoming urls and call more methods
-// todo. parse incoming urls and call more methods
-
 exports.addBlog = function(req, res) {
+    //extract the blogname
+    var blogName = req.params.blog;
 
+	/* Retrieve blog's likes. This will also tell us if the blog is valid 
+	//var posts = http://api.tumblr.com/v2/blog/blogName.tumblr.com/likes
+	//check if valid:
+	if (posts.meta.status == 404){
+		res.end('404! ' + blogName + ' Not Found.');
+	}*/
+	
+	//insert into database
+    mysql.addBlog(blogName);
+	console.log('new blog: ' + blogName);
+	
+	//now insert all the posts liked by this blog in likedPosts
+
+	//response: successful
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	res.end(content, 'utf-8');
 }
 
 // testing routing and db connection:
 // todo. replace with real functility
 exports.getAllTrends = function(req, res) {
-    mysql.getallblogs(); // example call to method exported from ./nodedb.js
-    res.send("here's your gundam response!");
+    //mysql.getallblogs(); // example call to method exported from ./nodedb.js
+    //res.send("here's your gundam response!");
+	
 }
 
 exports.getBlogTrends = function(req, res) {
--- 2/server.js
+++ 2/server.js
@@ -1,16 +1,15 @@
 var express = require('express');
+var params = require('express-params');
 var blogs = require('./blogs');
 var server = express();
 
 server.use(express.logger("dev"));
 // server.use(express.static(__dirname));
 
-server.get("/", blogs.frontDesk);
-
 server.get('/blog/:baseHostname/trends', blogs.getBlogTrends);
 server.get('/blogs/trends', blogs.getAllTrends);
 
-server.post('/blog', blogs.addBlog);
+server.post('/:blog', blogs.addBlog);
 
 var port = 30975;
 server.listen(port, function() {
