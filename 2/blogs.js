bde1ad9dcce16ee7868a09e7df7dd8df9bb6d3b9
Trong Truong <nahn.truong@gmail.com>
2013-03-02 00:23:46 -0500

    routing get urls to blogs.getPostsTracks

--- 2/blogs.js
+++ 2/blogs.js
@@ -23,52 +23,36 @@ exports.addBlog = function(req, res) {
 	res.end(content, 'utf-8');
 }
 
-/*
- * Return a JSON of all posts with largest increment (if trending)
- * or with most recent post time (if recent).
- */
-exports.getAllTrends = function(req, res) {
+// /*
+//  * Return a JSON of all posts with largest increment (if trending)
+//  * or with most recent post time (if recent).
+//  */
+// exports.getAllTrends = function(req, res) {
 
-	var order = req.query.order;
-	var limit = req.query.limit;
-	if (!limit){
-		limit = 10; //set default;
-	}
-	console.log('get all trending with order: ' + order + ' and limit ' + limit);
+// 	var order = req.query.order;
+// 	var limit = req.query.limit;
+// 	if (!limit){
+// 		limit = 10; //set default;
+// 	}
+// 	console.log('get all trending with order: ' + order + ' and limit ' + limit);
 
-	if (order == 'Trending'){
-		mysql.getAllTrending(res, limit);
-	} else if (order == 'Recent') {
-		mysql.getAllRecent(res, limit);
-	} else {
-		res.writeHead(400, "Incorrect order argument", {
-			'Content-Type': 'text/html'
-		});
-		res.end('404: Incorrect order argument. Please try "Trending" or "Recent".');
-	}
-}
+// 	if (order == 'Trending'){
+// 		mysql.getAllTrending(res, limit);
+// 	} else if (order == 'Recent') {
+// 		mysql.getAllRecent(res, limit);
+// 	} else {
+// 		res.writeHead(400, "Incorrect order argument", {
+// 			'Content-Type': 'text/html'
+// 		});
+// 		res.end('404: Incorrect order argument. Please try "Trending" or "Recent".');
+// 	}
+// }
 
-exports.getBlogTrends = function(req, res) {
+// todo. input validation
+exports.getPostsTracks = function(req, res) {
     var bh = req.params.baseHostname;
     var limit = req.query.limit;
     var order = req.query.order;
-
     mysql.getPostsTracks(res, bh, order, limit);
-
-    // // todo
-    // // todo
-    // // todo
-
-    // if (order == "Recent"){
-    //     if (!limit) {
-    //         var limit = 20; //default value
-    //     }
-    //     mysql.getBlogRecent(res, bh, order, limit);
-    // } else if (order == "Trending"){
-    //     if (!limit) {
-    //         var limit = 20; //default value
-    //     }
-    //     mysql.getBlogTrending(res, bh, order, limit);
-    // }
 }
 
--- 2/server.js
+++ 2/server.js
@@ -14,8 +14,8 @@ server.use(express.logger("dev"));
 server.use(express.bodyParser());
 
 server.get("/", blogs.frontDesk);
-server.get('/blog/:baseHostname/trends', blogs.getBlogTrends);
-server.get('/blogs/trends', blogs.getAllTrends);
+server.get('/blog/:baseHostname/trends', blogs.getPostsTracks);
+server.get('/blogs/trends', blogs.getPostsTracks);
 server.post('/blog', blogs.addBlog);
 
 var port = 30975;
