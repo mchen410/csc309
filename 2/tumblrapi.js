825b69ad11b28dd0cfb6d2f1d041c2ca8e452ef2
Truong Trong <c9truong@cdf.toronto.edu>
2013-03-10 23:28:35 -0400

    adding another check for json parsing 404 html response

--- 2/tumblrapi.js
+++ 2/tumblrapi.js
@@ -2,47 +2,51 @@ var http = require('http');
 var mysql = require('./nodedb');
 
 var tumblrGet = {
-	host: 'api.tumblr.com',
-	path: '/v2/blog/',
-	method: 'GET',
-	headers: {
-		'Content-Type': 'application/json'
-	}
+    host: 'api.tumblr.com',
+    path: '/v2/blog/',
+    method: 'GET',
+    headers: {
+	'Content-Type': 'application/json'
+    }
 };
 
 var apiKey = 'SR4t4ORhyC4rGwrs9uNIweXS6nE7nGtOLMuZLacKUoSlJIDKuC';
 
 exports.retrieveLikes = function(blog, ID){
-	console.log('Getting from http://api.tumblr.com/v2/blog/' + blog + '/' + 'likes')
+    console.log('Getting from http://api.tumblr.com/v2/blog/' + blog + '/' + 'likes')
 
-	//Copy tumblrGet
-	var options = {};
-	for( var key in tumblrGet)
+    //Copy tumblrGet
+    var options = {};
+    for( var key in tumblrGet)
     	options[key] = tumblrGet[key];
 
-	//Append blog id to the options variable and add the API key
-	options['path'] += blog + '/likes?api_key=' + apiKey;
+    //Append blog id to the options variable and add the API key
+    options['path'] += blog + '/likes?api_key=' + apiKey;
 
-	var output = '';
+    var output = '';
 
-	//Make GET request to Tumblr
-	var request = http.request(options, function(response) {
-		response.setEncoding('utf8');
+    //Make GET request to Tumblr
+    var request = http.request(options, function(response) {
+	response.setEncoding('utf8');
 
-		response.on('data', function(chunk) {
-			output += chunk;
-		});
+	response.on('data', function(chunk) {
+	    output += chunk;
+	});
 
-		response.on('end', function() {
-			var json = JSON.parse(output);
-			// console.log(json);
+	response.on('end', function() {
+	    try {
+		var json = JSON.parse(output);
+		// console.log(json);
+	    } catch (e){
+		return;
+	    }
             if (json.meta.status == "200"){
-			    mysql.handlePosts(json, ID);
+		mysql.handlePosts(json, ID);
             } else {
                 console.log("tumblr request error");
             }
-		});
 	});
+    });
 
-	request.end();
+    request.end();
 };
